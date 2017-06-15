de6b66e730c498d075bf36cd7cd99a1f046de198
Jonathan Nguyen <JNguyen@paraflow.bg>
2017-06-15 17:13:51 +0300

    push -u

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $(function(){
     var $friends = $('#friends');
     var $name = $('#name');
     var $age = $('#age');
+    var $photos = $('#photos');
+    var $albumId = $('#albumId');
 
     /*Mustache template*/
     var friendTemplate = 
@@ -11,6 +13,36 @@ $(function(){
     "<p><h4>Age: <strong>{{age}}</strong></h4></p>"+ 
     "<p class='text-right'><button type='submit' class='remove btn btn-default' data-id='{{id}}'><span class='glyphicon glyphicon-remove'></span></button></p>" +
     "</li>";
+
+    /*Mustache template*/
+    var photoTemplate = 
+    "<li id='{{id}}'>"+
+        "<div>" +
+            "<div class='container col-md-4'>" +
+                    "<img src='{{thumbnailUrl}}' class=' img-circle'></img>" +
+            "</div>" +
+
+            "<div class='row'>" +
+                "<div class='container col-md-6'>" +
+                    "<p><h4>Title:{{title}}</h4></p>"+ 
+                "</div>" +
+                
+            "</div>" +
+        "</div>" +
+    "</li>" +
+    "<br>";
+
+    /*Get pictures from API*/
+    $.ajax({
+    type: 'GET',
+    url: 'https://jsonplaceholder.typicode.com/photos',
+    success: function(data) {
+        $.each(data,function(i,photo) {
+            if(photo.id < 10)
+            $photos.append(Mustache.render(photoTemplate,photo));      
+        });
+    }
+    });
 
     /*Filling the ul with items which are not empty*/
     $.ajax({
@@ -30,30 +62,30 @@ $(function(){
             name: $name.val(),
             age: $age.val(),
         };
+
+            $.ajax({
+                type:'POST',
+                url: 'http://rest.learncode.academy/api/johnbob/friends/',
+                data: $addFriend,
+                success:function(addFriend) {
+                    $("#friends").append(Mustache.render(friendTemplate,addFriend));
+                }
+            });
         
-        $.ajax({
-            type:'POST',
-            url: 'http://rest.learncode.academy/api/johnbob/friends/',
-            data: $addFriend,
-            success:function(addFriend) {
-                $("#friends").append(Mustache.render(friendTemplate,addFriend));
-            },
-            error: function(){
-                alert('Error adding friend!');
-            }
-        });
-        
-    })
+    });
 
     /*Deleting the item with data-id in API,also deleting the li on button click within the li*/
     $friends.delegate(".remove", "click" ,function() {
             var $li = $(this).closest('li');
-
+            var self = this;
             $.ajax({
             type: 'DELETE',
             url: 'http://rest.learncode.academy/api/johnbob/friends/' + $(this).attr('data-id'),       
             success: function() {
-                $li.remove();
+                $(self);
+                $li.fadeOut(300,function(){
+                    $(this).remove();
+                });
             },
             error: function(){
                 alert('Error removing friend!');
